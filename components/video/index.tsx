d8f933bc28e74dbd1ad3dc9843ef7b4b343bcac8
timoransky <jan.timoransky@brackets.sk>
2023-04-05 09:08:59 +0200

    fix

--- components/video/index.tsx
+++ components/video/index.tsx
@@ -1,7 +1,7 @@
 export default function Video({ src, alt, ...props }) {
   return (
     <div className="shadow-sm shadow-slate-100 rounded-xl overflow-hidden border border-slate-200">
-      <video src={src} type="video/mp4" controls muted />
+      <video src={src} controls muted />
     </div>
   );
 }
