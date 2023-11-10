7d19c266ad1eab96cce483f34399d415fd796fba
Dmitry Bobryshev <dmitry.bobryshev@brackets.sk>
2023-11-10 16:33:58 +0100

    :sparkles:  feat(introduction): create tech media intro

--- components/image/index.tsx
+++ components/image/index.tsx
@@ -1,7 +1,13 @@
-export default function Image({ src, alt, ...props }) {
+export default function Image({ src, alt, width, height, short, ...props }) {
   return (
-    <div className="shadow-sm shadow-slate-100 rounded-xl overflow-hidden border border-slate-200">
-      <img alt={alt} src={src} />
+    <div
+      className={`${
+        !short
+          ? 'shadow-sm shadow-slate-100 rounded-xl overflow-hidden border border-slate-200'
+          : 'opacity-70'
+      }`}
+    >
+      <img alt={alt} src={src} width={width} height={height} />
     </div>
   );
 }
